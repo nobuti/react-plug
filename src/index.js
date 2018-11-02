0695aa034b3ec654d0d4d90d883c09733ffb066d
nobuti <buti@nobuti.com>
2018-11-02 07:57:30 +0100

    Fix release

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import useWindowSize from './hooks/useWindowSize'
 import useKeypress from './hooks/useKeypress'
 import useOnline from './hooks/useOnline'
 import usePageHidden from './hooks/usePageHidden'
+import useMount from './hooks/useMount'
+import useUnMount from './hooks/useUnMount'
 
-export { useWindowSize, useKeypress, useOnline, usePageHidden }
+export { useWindowSize, useKeypress, useOnline, usePageHidden, useMount, useUnMount }
