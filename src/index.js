bf7a7881a72ea800ba450a2c7ebd2dc37e87b9f0
nobuti <buti@nobuti.com>
2019-11-18 11:55:03 +0100

    Export useResizeObserver

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import usePageHidden from './hooks/usePageHidden'
 import useMount from './hooks/useMount'
 import useUnMount from './hooks/useUnMount'
 import useScrollPosition from './hooks/useScrollPosition'
+import useResizeObserver from './hooks/useResizeObserver'
 
-export { useWindowSize, useKeypress, useOnline, usePageHidden, useMount, useUnMount, useScrollPosition }
+export { useWindowSize, useKeypress, useOnline, usePageHidden, useMount, useUnMount, useScrollPosition, useResizeObserver }
